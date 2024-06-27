8e6a16e566072d7c02132481cdf8c3367e4b820c
Gleb Ginko <glebatyginko@yandex.ru>
2024-06-27 19:52:53 +0300

    first version

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,39 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(addCard, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
+
+  cardImage.src = addCard.link;
+  cardImage.alt = addCard.name;
+  cardTitle.textContent = addCard.name;
+  
+  cardDeleteButton.addEventListener('click', () => {
+    deleteCard(cardElement);
+  });
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(card) {
+  card.remove();
+}
 
 // @todo: Вывести карточки на страницу
+function addCards(cards) {
+  cards.forEach(card => {
+    const cardItem = createCard(card, deleteCard);
+    cardList.append(cardItem);
+  });
+}
+
+addCards(initialCards);
